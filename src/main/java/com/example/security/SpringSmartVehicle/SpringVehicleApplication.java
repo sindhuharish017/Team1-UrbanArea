a99b823c5050f45bb66289632e1e002b7b79ef51
Banapili Saikumar <banapili.saikumar@valtech.com>
2023-01-04 18:44:18 +0530

    deployed war file

--- src/main/java/com/example/security/SpringSmartVehicle/SpringVehicleApplication.java
+++ src/main/java/com/example/security/SpringSmartVehicle/SpringVehicleApplication.java
@@ -2,10 +2,16 @@ package com.example.security.SpringSmartVehicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringVehicleApplication {
+public class SpringVehicleApplication extends  SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringVehicleApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(SpringVehicleApplication.class, args);
 	}
